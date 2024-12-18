2591678bccabb6a108d0a02211890551b0e4f524
nikitazapekin <wotblitz362@mail.ru>
2024-12-18 16:31:51 +0300

    last

--- index.js
+++ index.js
@@ -22,29 +22,9 @@ const modal = document.querySelector(".modal")
 infoBtn.addEventListener("click", () => {
     modal.classList.remove("modal__close")
 })
-modalOverlay.addEventListener("click", ()=>{
+modalOverlay.addEventListener("click", () => {
     modal.classList.add("modal__close")
 })
-modalBtn.addEventListener("click", ()=>{
+modalBtn.addEventListener("click", () => {
     modal.classList.add("modal__close")
 })
-
-/* info__btn
- <div class="modal modal__close">
-                <form class="modal__inner">
-                    <input type="text" class="modal__input" 
-                    placeholder="ваше имя..." required
-                    >
-                    <input type="text" class="modal__input"
-                    placeholder="номер телефона.." required
-                    >
-                    <input type="text" class="modal__input"
-                    placeholder="email адрес..." required
-                    >
-                   <button class="form__submit">
-                    Отправить
-                   </button>
-                </form>
-
-                <div class="modal__background"></div>
-*/
